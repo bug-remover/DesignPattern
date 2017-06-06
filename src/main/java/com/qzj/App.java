98198a408c031bc3f03ae2e51a4896bd5ad68e60
qizhongju <15618796966@163.com>
2017-06-06 16:14:13 +0800

    maven项目初始化

---
+++ src/main/java/com/qzj/App.java
@@ -0,0 +1,13 @@
+package com.qzj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
